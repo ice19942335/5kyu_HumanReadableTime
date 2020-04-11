599ae0ccd215eb53eba33d3eaed08a8d72f1c216
ice19942335 <ice19942335@gmail.com>
2020-04-11 17:11:17 +0100

    Add project files.

---
+++ 5kyu_HumanReadableTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5kyu_HumanReadableTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetReadableTime(0);
+            GetReadableTime(5);
+            GetReadableTime(60);
+            GetReadableTime(86399);
+            GetReadableTime(359999);
+        }
+
+        public static string GetReadableTime(int seconds)
+        {
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", seconds / 3600, seconds / 60 % 60, seconds % 60);
+        }
+    }
+}
